48e03fc6bfda7e719cbd6c7cb902d5ac4ca791b7
MatthewLowrie <98457937+MatthewLowrie@users.noreply.github.com>
2023-04-26 02:12:10 +0100

    Update playlist_generator.py

--- Matthews Files/playlist_generator.py
+++ Matthews Files/playlist_generator.py
@@ -1,62 +1,131 @@
 import requests
 
-endpoint_url = "https://api.spotify.com/v1/recommendations?"
-TOKEN = 'BQC5uC4I5YtM__ke3gkmLeJlYYaWjqMQT2aDU6Eqi1TmBft73rRn8rFuUb8bfz6ufyFmrs-a38rhjmRzKIp7bdGknSTd1DhoCQqpVbP1NJJYVxYqt5BuvRRj_jzBCiE8fQs3Bi4ordAar5z5WVJCZTkDvEGnjXMOeDBTBc5fSxVXyCeRqZ6-3quEYhjH64paJmWriCdz9R2ri8qOO6Hv_f9g3TkPiGacJtNfqSxfwED3_4_lABdiode7kpgVjBJzu2vw8-vNPK3RC-KL_Z2DwP_QcUyNQCIIriP9KK7-wlMyK4Q-L0GiKPCvfQ-HG0EkPXOqTgBTqYIDChfrfOD4xPE5vrZOvyOV'   #TOKEN
-user_id = 'reesespieces07' #USER ID
-
-########################################################################################################
-
-
-# I MADE A NEW RECCOMENDATION ENGINE FOR THE TEST BUT THIS SHOULD BE ABLE TO EB DELETED FROM THIS FILE
-
-# OUR FILTERS
-limit= 50
-market="GB"
-seed_genres="hip-hop"
-target_danceability=0.9
-seed_artists = '2jku7tDXc6XoB6MO2hFuqg'
-seed_tracks='7n0lXKEOaFxrAU0R93fIQh'
-query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}'
-query += f'&seed_artists={seed_artists}'
-query += f'&seed_tracks={seed_tracks}'
-
-query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}'
-
-response =requests.get(query, 
-               headers={"Content-Type":"application/json", 
-                        "Authorization":"Bearer {TOKEN}".format(TOKEN=TOKEN)})
-
-json_response = response.json()
-
-uris = [] 
-
-for i in json_response['tracks']:
-            uris.append(i['uri'])
-            print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
-            
-############################################################
-
-#THIS IS THE PART WE CAN ADD ON TO OUR PREVIOUS RECCOMENDATION FILE
-            
+import sqlalchemy
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+import requests
 import json
+from datetime import datetime
+import datetime 
+import sqlite3
 
-endpoint_url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
-request_body = json.dumps({
-          "name": "Metronome",
-          "description": "This playlist was generated in a data pipeline!",
-          "public": False # let's keep it between us - for now
-        })
-response = requests.post(url = endpoint_url, data = request_body, headers={"Content-Type":"application/json", 
-                        "Authorization":"Bearer {TOKEN}".format(TOKEN=TOKEN)})
 
-playlist_id = response.json()['id']
 
-last_endpoint_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
 
-request_body = json.dumps({
-          "uris" : uris
-        })
-response = requests.post(url = last_endpoint_url, data = request_body, headers={"Content-Type":"application/json", 
-                        "Authorization":"Bearer {token}".format(token=TOKEN)})
 
-print(response.status_code)  # IF CODE 201 IS DISPLAYED IT MEANS IT WORKS
+def get_seed():
+  import random
+  import pandas as pd
+
+  # Open the file and read its contents into a list of strings
+  with open('songids.txt', 'r') as file:
+      lines = file.readlines()
+      # Strip the newline character from each line
+      lines = [line.strip() for line in lines]
+
+  # Choose a random line from the list
+  #this choses a random track to use as a seed track
+  random_song = random.choice(lines)
+  get_reccomendations(random_song)
+  
+
+
+def get_reccomendations(random_song):
+  
+  endpoint_url = "https://api.spotify.com/v1/recommendations?"
+  user_id = "7gau3mfurpamvofd00ej790p9" 
+  TOKEN = "BQCuvxjON0t65E_8IR4e1TBzB4nPOAeHJN1v0PBIALtUDwHLmCeWYDBPlQdyyC8wJDC3FukRHzaBnorZf0y-COuUMUcPrpvMxvc6fte_ysSodWz1HkRwKgHtq-0mZRoxeLwcD6-v26csQksEpzUAtBY6XdOA6y_JeDf0i4dGwZ_TcqrDuV65jw9eQQdx_-lxTr4tLq0TDcjQW7mVT2BgFmoSVFD7sJ8y2yPh2JR9bUcMZe0rhyZrHKnpr0kDK8yzpDUIcM8QOxUBcPmTw2l37Z7o7WBjGORp51mNUWM4T1lcGNNiDVCodp8AU21sb6EpH1QHWSmFB_Nvx9VYP2vsHGS6A2Cer5oZj_uFBqeI1M9U_BI"
+  # OUR FILTERS
+  limit= 50
+  market="US"
+  seed_genres="hip-hop"
+  seed_artists = ''
+  target_danceability=0.9
+  seed_tracks= random_song # ID for Cassius by Foals
+  query = f'{endpoint_url}limit={limit}&market={market}&target_danceability={target_danceability}'
+  #query += f'&seed_artists={seed_artists}'
+  query += f'&seed_tracks={seed_tracks}'
+
+
+
+
+
+  response =requests.get(query, 
+                headers={"Content-Type":"application/json", 
+                          "Authorization":"Bearer {TOKEN}".format(TOKEN=TOKEN)})
+
+
+
+
+  json_response = response.json()
+
+
+  uris = []
+  name = []
+  song_name = []
+  song_uri=[]
+
+  for i in json_response['tracks']:
+              uris.append(i['uri'])
+              print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
+              song_name = []
+              name = []
+              song_uri = []
+              
+  song_dict = {"Artist Name": name, "Song Name": song_name, 'uris' : song_uri}
+
+  # Convert the dictionary into a DataFrame
+  recc_df = pd.DataFrame(song_dict)
+  make_db(recc_df)
+  make_playlist(user_id, TOKEN,uris) 
+
+
+
+def make_db(recc_df):
+       
+  # Define the file path and database name
+  db_path = "reccomendations.db"
+  # Create a SQLite database connection
+  conn = sqlite3.connect(db_path)
+
+  # Create the recommendations table in the database
+  c = conn.cursor()
+  c.execute('''CREATE TABLE IF NOT EXISTS recommendations
+          (song_name text, name text)''')
+  
+
+  # Insert the DataFrame into the recommendations table
+  recc_df.to_sql('recommendations', conn, if_exists='replace', index=False)
+
+  # Commit the changes and close the database connection
+  conn.commit()
+  conn.close()
+            
+           
+
+def make_playlist(user_id, TOKEN,uris):
+  endpoint_url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+  request_body = json.dumps({
+            "name": "Indie bands like Franz Ferdinand but using Python",
+            "description": "My first programmatic playlist, yooo!",
+            "public": False # let's keep it between us - for now
+          })
+  response = requests.post(url = endpoint_url, data = request_body, headers={"Content-Type":"application/json", 
+                          "Authorization":"Bearer {TOKEN}".format(TOKEN=TOKEN)})
+
+
+
+  playlist_id = response.json()['id']
+
+
+  last_endpoint_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+
+  request_body = json.dumps({
+            "uris" : uris
+          })
+  response = requests.post(url = last_endpoint_url, data = request_body, headers={"Content-Type":"application/json", 
+                          "Authorization":"Bearer {TOKEN}".format(TOKEN=TOKEN)})
+
+  print(response.status_code)
+  
+get_seed()
